1e8fcace2efc469392dc30fffbc553a7843c3368
Peter54-WW <1225100316.pgm@gmail.com>
2026-09-24 15:48:10 -0600

    Recursividad imprimiendo 10 veces con nombre2

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -1,19 +1,19 @@
 package datos.unidad1.recursividad;
 public class Recursividad {
 	
-	public static void saludo(int total) {
+	public static void saludo(int total, String nombre) {
 	
 	if(total <= 0)
 		return;
 	else{
-		System.out.println("Hola Pedro");
-		saludo(total-1);
+		System.out.println("Hola " + nombre);
+		saludo(total-1, nombre);
 	}
 	
 	}
 	
 	public static void main(String[] args){
-		saludo(10);
+		saludo(10, "Pedro");
 	}
 
 }
