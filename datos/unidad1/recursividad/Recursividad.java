20bca69e7628c2c941bd4b357f80e528d6318be7
Peter54-WW <1225100316.pgm@gmail.com>
2026-09-25 14:48:37 -0600

    Suma de arrglo con recursividad

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -27,10 +27,21 @@ public class Recursividad {
 			cuentaRegresiva(n-1);
 		}
 	}
+
+	
+	public static int sumaRecursivia(int[] data, int longitud){
+		if(longitud <= 0){
+			return 0;
+		} else {
+			return data[longitud-1] + sumaRecursivia(data, longitud - 1);
+		}
+	}
 	
 	public static void main(String[] args){
 		//saludo(100, "Pedro");
-		cuentaRegresiva(100);
+		//cuentaRegresiva(100);
+		int[] data = {3,4,5,6,7,8};
+		System.out.println(sumaRecursivia(data, data.length));
 	}
 
 }
